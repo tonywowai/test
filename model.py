5effc53940156c0772dfc21d53d9e823d7252243
tonywowai <tonyshark365@gmail.com>
2024-06-10 21:55:47 +0700

    .

--- model.py
+++ model.py
@@ -119,23 +119,23 @@ window.addEventListener("DOMContentLoaded", function() {
 });
 """
 
-def download_checkpoint(weight_zip_path, weights_path, project_id, checkpoint_id, token):
+def download_checkpoint(weight_zip_path, project_id, checkpoint_id, token):
     url = f"{HOST_NAME}/api/checkpoint_model_marketplace/download/{checkpoint_id}?project_id={project_id}"
-    print(url)
     payload = {}
     headers = {
-    'accept': 'application/json',
-    # 'Authorization': 'Token 5d3604c4c57def9a192950ef7b90d7f1e0bb05c1'
-    'Authorization': f'Token {token}'
+        'accept': 'application/json',
+        # 'Authorization': 'Token 5d3604c4c57def9a192950ef7b90d7f1e0bb05c1'
+        'Authorization': f'Token {token}'
     }
     response = requests.request("GET", url, headers=headers, data=payload) 
+    checkpoint_name = response.headers.get('X-Checkpoint-Name')
+
     with open(weight_zip_path, 'wb') as f:
         f.write(response.content)
 
-    with zipfile.ZipFile(weight_zip_path, 'r') as zip_ref:
-        zip_ref.extractall(weights_path)
+    return checkpoint_name
 
-def download_dataset(data_zip_dir, dataset_path, project_id, dataset_id, token):
+def download_dataset(data_zip_dir, project_id, dataset_id, token):
     # data_zip_path = os.path.join(data_zip_dir, "data.zip")
     url = f"{HOST_NAME}/api/dataset_model_marketplace/download/{dataset_id}?project_id={project_id}"
     payload = {}
@@ -145,11 +145,11 @@ def download_dataset(data_zip_dir, dataset_path, project_id, dataset_id, token):
     }
 
     response = requests.request("GET", url, headers=headers, data=payload) 
+    dataset_name = response.headers.get('X-Checkpoint-Name')
     with open(data_zip_dir, 'wb') as f:
         f.write(response.content)
     
-    with zipfile.ZipFile(data_zip_dir, 'r') as zip_ref:
-        zip_ref.extractall(dataset_path)
+    return dataset_name
 
 def upload_checkpoint(checkpoint_model_dir, project_id, token):
     url = f"{HOST_NAME}/api/checkpoint_model_marketplace/upload/"
@@ -232,29 +232,34 @@ class MyModel(AIxBlockMLBase):
 
                 if kwargs.get("checkpoint"):
                     checkpoint_id = kwargs.get("checkpoint")
-                    weight_path = os.path.join(clone_dir, f"models/{checkpoint_id}")
                     weight_zip_path = os.path.join(clone_dir, "data_zip/weights.zip")
-                    print(weight_zip_path)
-                    download_checkpoint(weight_zip_path, weight_path, project_id, checkpoint_id, token)
+                    checkpoint_name = download_checkpoint(weight_zip_path, project_id, checkpoint_id, token)
+                    weight_path = os.path.join(clone_dir, f"models/{checkpoint_name}")
+                    if not os.path.exists(weight_path):
+                        with zipfile.ZipFile(weight_zip_path, 'r') as zip_ref:
+                            zip_ref.extractall(weight_path)
 
                 if  kwargs.get("dataset"):
                     dataset_id = kwargs.get("dataset")
                     data_zip_dir = os.path.join(clone_dir, "data_zip/data.zip")
-                    dataset_path = os.path.join(clone_dir, f"datasets/{dataset_id}") 
-                    download_dataset(data_zip_dir, dataset_path, project_id, checkpoint_id, token)
+                    dataset_name = download_dataset(data_zip_dir, project_id, dataset_id, token)
+                    dataset_path = os.path.join(clone_dir, f"datasets/{dataset_name}")
+                    if not os.path.exists(dataset_path):
+                        with zipfile.ZipFile(data_zip_dir, 'r') as zip_ref:
+                            zip_ref.extractall(dataset_path)
 
-                    data_train_dir = os.path.join(dataset_path, "data.yaml")
-                    with open(data_train_dir, 'r') as file:
-                        data_yaml = yaml.safe_load(file)
-                    
-                    # Thay thế các đường dẫn
-                    data_yaml['train'] = os.path.join('train', 'images')
-                    data_yaml['val'] = os.path.join('valid', 'images')
-                    data_yaml['test'] = os.path.join('test', 'images')
+                        data_train_dir = os.path.join(dataset_path, "data.yaml")
+                        with open(data_train_dir, 'r') as file:
+                            data_yaml = yaml.safe_load(file)
+                        
+                        # Thay thế các đường dẫn
+                        data_yaml['train'] = os.path.join('train', 'images')
+                        data_yaml['val'] = os.path.join('valid', 'images')
+                        data_yaml['test'] = os.path.join('test', 'images')
 
-                    # Ghi lại data.yaml
-                    with open(data_train_dir, 'w') as file:
-                        yaml.dump(data_yaml, file, default_flow_style=False, sort_keys=False)
+                        # Ghi lại data.yaml
+                        with open(data_train_dir, 'w') as file:
+                            yaml.dump(data_yaml, file, default_flow_style=False, sort_keys=False)
 
                 files = [os.path.join(weight_path, filename) for filename in os.listdir(weight_path) if os.path.isfile(os.path.join(weight_path, filename))]
                 if len(files) > 0:
